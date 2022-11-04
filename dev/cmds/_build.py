16cfafd046287723199d6f490fcfaeb5b9c41a98
Stefan van der Walt <stefanv@berkeley.edu>
2022-11-04 13:28:48 -0700

    Allow arbitrary arguments to be passed through to Meson

--- dev/cmds/_build.py
+++ dev/cmds/_build.py
@@ -10,10 +10,17 @@ from .util import run
     "--build-dir", default="build", help="Build directory; default is `$PWD/build`"
 )
 @click.option("-j", "--jobs", help="Nr of parallel tasks to launch", type=int)
-def build(build_dir, jobs=None):
-    """🔧 Build package with Meson/ninja"""
+@click.argument("meson_args", nargs=-1)
+def build(build_dir, meson_args, jobs=None):
+    """🔧 Build package with Meson/ninja
+
+    MESON_ARGS are passed through directly to pytest, e.g.:
+
+    ./dev.py build -- -Dpkg_config_path=/lib64/pkgconfig
+
+    """
     build_dir = os.path.abspath(build_dir)
-    build_cmd = ["meson", f"--prefix={build_dir}", "build"]
+    build_cmd = ["meson", f"--prefix={build_dir}", "build"] + list(meson_args)
     flags = []
 
     if os.path.exists(build_dir):
