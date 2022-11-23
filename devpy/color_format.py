1c04b9874285f5c66a830cc430951ea2f5bdd6b0
Stefan van der Walt <stefanv@berkeley.edu>
2022-11-23 10:35:30 -0800

    Refactor regexp color formatting

--- devpy/color_format.py
+++ devpy/color_format.py
@@ -1,33 +1,51 @@
 import re
+import shutil
 
 import click
 
 
+class RegexpFormatter:
+    def __init__(self, rules, default=None):
+        if default is None:
+            default = {}
+        self.rules = {re.compile(expr): format for (expr, format) in rules.items()}
+        self.default_style = click.style("", **default, reset=False)
+        self.reset_style = click.style("", reset=True)
+
+    def __call__(self, txt):
+        for (expr, format) in self.rules.items():
+            txt = expr.sub(click.style("\\g<0>", **format) + self.default_style, txt)
+        txt = self.default_style + txt + self.reset_style
+        return txt
+
+
 class ColorHelpFormatter(click.formatting.HelpFormatter):
+    def __init__(self, *args, **kwargs):
+        kwargs["width"] = shutil.get_terminal_size()[0]
+        super().__init__(*args, **kwargs)
+
     def write_heading(self, heading):
         super().write_heading(click.style(heading, dim=True))
 
     def write_dl(self, items):
-        default_style = click.style("", bold=True, fg="cyan", reset=False)
-        reset_style = click.style("", reset=True)
-        style_rules = {
-            " [A-Z]+": {"bold": True, "fg": "yellow"},
-            r"\-[a-z]{1}(?=[ ,]{1})": {"fg": "green"},
-        }
-        style_rules = {
-            re.compile(expr): format for (expr, format) in style_rules.items()
-        }
-        for (expr, format) in style_rules.items():
-            items = [
-                (expr.sub(click.style("\\g<0>", **format) + default_style, key), val)
-                for (key, val) in items
-            ]
-        items = [(default_style + key, reset_style + val) for (key, val) in items]
+        """Print definition list"""
+        key_fmt = RegexpFormatter(
+            {
+                " [A-Z]+": {"bold": True, "fg": "yellow"},
+                r"\-[a-z]{1}(?=[ ,]{1})": {"fg": "green"},
+            },
+            default={"bold": True, "fg": "cyan"},
+        )
+        val_fmt = RegexpFormatter({r"\[default: .*?\]": {"dim": True}})
+        items = [(key_fmt(key), val_fmt(val)) for (key, val) in items]
         super().write_dl(items)
 
     def write_usage(self, prog, args="", prefix=None):
+        fmt = RegexpFormatter(
+            {r"(?<=\[)[A-Z_]+(?=\])": {"fg": "cyan"}}, default={"bold": True}
+        )
         super().write_usage(
             click.style(prog, bold=True, fg="white"),
-            args,
+            click.style(fmt(args), bold=True),
             prefix=click.style("Usage: ", bold=True, fg="yellow"),
         )
