8a398ae03429b58cc15b63a3f37f9b10d01aaf1d
Jarrod Millman <jarrod.millman@gmail.com>
2023-06-27 22:21:30 -0700

    Test on Python 3.12.0-beta.2 (#73)
    * Test on Python 3.12.0-beta.2
    
    * Fix config dictionary for Python 3.12
    
    ---------
    
    Co-authored-by: Stefan van der Walt <stefanv@berkeley.edu>

--- spin/__main__.py
+++ spin/__main__.py
@@ -26,6 +26,17 @@ class DotDict(collections.UserDict):
                 raise KeyError(f"`{key}` not found in configuration") from None
         return subitem
 
+    # Fix for Python 3.12
+    # See https://github.com/python/cpython/issues/105524
+    def __contains__(self, key):
+        subitem = self.data
+        for subkey in key.split("."):
+            try:
+                subitem = subitem[subkey]
+            except KeyError:
+                return False
+        return True
+
 
 def main():
     if not os.path.exists("pyproject.toml"):
