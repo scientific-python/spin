1f7e495358b590230213e847548041ea1dd54276
Stefan van der Walt <stefanv@berkeley.edu>
2023-06-30 20:38:09 -0700

    Be even more explicit

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -10,9 +10,10 @@ prun spin sdist
 prun spin example
 prun spin docs
 
+echo SPIN_PYTHONPATH=\$\(spin run 'echo $PYTHONPATH'\)
 SPIN_PYTHONPATH=$(spin run 'echo $PYTHONPATH')
 echo spin sees PYTHONPATH=\"${SPIN_PYTHONPATH}\"
-$ if [[ ${SPIN_PYTHONPATH} == "\$PYTHONPATH" ]]; then
+if [[ ${SPIN_PYTHONPATH} == "\$PYTHONPATH" ]]; then
     echo -n "\!\!\!\!\n\nIf this says \$PYTHONPATH, that's an error\n\n\!\!\!\!\n"
 fi
 [[ ${SPIN_PYTHONPATH} == *"site-packages" ]]
