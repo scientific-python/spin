3822a6f86006d6d4a1306bffe13f7a237ae3df7e
Stefan van der Walt <stefanv@berkeley.edu>
2023-07-01 09:26:12 -0700

    Show how to access defined commands in example command

--- example_pkg/.spin/cmds.py
+++ example_pkg/.spin/cmds.py
@@ -16,9 +16,14 @@ def example(flag):
     click.secho("Running example custom command", bold=True, fg="bright_blue")
     print()
     config = util.get_config()
-    util.get_commands()
+    commands = util.get_commands()
     click.secho("Flag provided with --flag is: ", fg="yellow", nl=False)
     print(flag or None)
+
+    click.secho("\nDefined commands:", fg="yellow")
+    for section in commands:
+        print(f"  {section}: ", end="")
+        print(", ".join(cmd.name for cmd in commands[section]))
 
     click.secho("\nTool config is:", fg="yellow")
     print(json.dumps(config["tool.spin"], indent=2))
