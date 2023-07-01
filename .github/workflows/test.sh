7cd658a5abc177881bb052654cc65972767398d1
Stefan van der Walt <stefanv@berkeley.edu>
2023-06-30 21:22:25 -0700

    Shuffle tests

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -5,11 +5,8 @@ prun() { echo "\$ $@" ; "$@" ; }
 prun cd example_pkg
 
 prun spin build
-prun spin test
-prun spin sdist
-prun spin example
-prun spin docs
 
+# Test spin run
 echo SPIN_PYTHONPATH=\$\(spin run 'echo $PYTHONPATH'\)
 SPIN_PYTHONPATH=$(spin run 'echo $PYTHONPATH')
 echo spin sees PYTHONPATH=\"${SPIN_PYTHONPATH}\"
@@ -17,5 +14,9 @@ if [[ ${SPIN_PYTHONPATH} == "\$PYTHONPATH" ]]; then
     echo -n "\!\!\!\!\n\nIf this says \$PYTHONPATH, that's an error\n\n\!\!\!\!\n"
 fi
 [[ ${SPIN_PYTHONPATH} == *"site-packages" ]]
-
 prun spin run python -c 'import sys; del sys.path[0]; import example_pkg; print(example_pkg.__version__)'
+
+prun spin test
+prun spin sdist
+prun spin example
+prun spin docs
