77a7283b317c4f89ac8cbb6d49071ed7a8f6f8cf
Stefan van der Walt <stefanv@berkeley.edu>
2023-09-14 13:56:21 -0700

    Ensure `spin run` echoes only command output to stdout (#109)
    Also clean up test output to make it easier to follow

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,20 +1,37 @@
 set -e
 
-prun() { echo "\$ $@" ; "$@" ; }
+RED="\033[31;1m"
+MAGENTA="\033[35m"
+NORMAL="\033[0m"
+
+prun() { echo -e "\n$RED\$ $@ $NORMAL\n" ; "$@" ; }
 
 prun cd example_pkg
 
 prun spin build
 
 # Test spin run
-echo SPIN_PYTHONPATH=\$\(spin run 'echo $PYTHONPATH'\)
 SPIN_PYTHONPATH=$(spin run 'echo $PYTHONPATH')
 echo spin sees PYTHONPATH=\"${SPIN_PYTHONPATH}\"
 if [[ ${SPIN_PYTHONPATH} == "\$PYTHONPATH" ]]; then
-    echo -n "\!\!\!\!\n\nIf this says \$PYTHONPATH, that's an error\n\n\!\!\!\!\n"
+    echo "Expected Python path, but got $SPIN_PYTHONPATH instead"
+    exit 1
 fi
-[[ ${SPIN_PYTHONPATH} == *"site-packages" ]]
-prun spin run python -c 'import sys; del sys.path[0]; import example_pkg; print(example_pkg.__version__)'
+echo -e "${MAGENTA}Does \$PYTHONPATH contains site-packages?${NORMAL}"
+if [[ ${SPIN_PYTHONPATH} == *"site-packages" ]]; then
+    echo "Yes"
+else
+    echo "No; it is $SPIN_PYTHONPATH"
+fi
+echo -e "${MAGENTA}Does \`spin run\` redirect only command output to stdout?${NORMAL}"
+# Once we're on Python >3.11, can replace syspath manipulation below with -P flag to Python
+VERSION=$(spin run python -c 'import sys; del sys.path[0]; import example_pkg; print(example_pkg.__version__)')
+if [[ $VERSION == "0.0.0dev0" ]]; then
+    echo "Yes"
+else
+    echo "No, output is $VERSION"
+    exit 1
+fi
 
 prun spin test
 prun spin sdist
