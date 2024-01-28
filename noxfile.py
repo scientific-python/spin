8436d68f3730745eaadd87ee0a83ce9f6674b0e2
ganesh-k13 <ganesh3597@gmail.com>
2024-01-28 10:19:18 +0530

    MAINT, WIP: Trying to fix dependencies

--- noxfile.py
+++ noxfile.py
@@ -2,10 +2,17 @@ import nox
 
 
 @nox.session
-def tests(session: nox.Session) -> None:
+def tests_old_sh(session: nox.Session) -> None:
     """
-    Run the unit and regular tests.
+    [TBD DEPCRECATED]Run the unit and regular tests.
+    """
+    session.install(".", "pytest", "build")
+    session.run("bash", ".github/workflows/test.sh", external=True)
+
+@nox.session
+def tests_pytest(session: nox.Session) -> None:
+    """
+    [TBD DEPCRECATED]Run the unit and regular tests.
     """
     session.install(".", "pytest", "build")
     session.run("pytest", "spin", *session.posargs)
-    session.run("bash", ".github/workflows/test.sh", external=True)
