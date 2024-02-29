9749de0dcda2e4707cc03eb920f51963e4e7a159
ganesh-k13 <ganesh3597@gmail.com>
2024-02-29 12:37:28 +0530

    TST: Added UT for `gcov-report`

--- noxfile.py
+++ noxfile.py
@@ -3,5 +3,5 @@ import nox
 
 @nox.session
 def test(session: nox.Session) -> None:
-    session.install(".", "pytest", "build", "meson-python", "ninja")
+    session.install(".", "pytest", "build", "meson-python", "ninja", "gcovr")
     session.run("pytest", "spin", *session.posargs)
