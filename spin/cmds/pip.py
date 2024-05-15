88457cdd029a70b85901815f3cebd633619cc0af
Stefan van der Walt <stefanv@berkeley.edu>
2024-05-15 12:33:58 -0700

    Make meson import build verbosity implicit

--- spin/cmds/pip.py
+++ spin/cmds/pip.py
@@ -5,20 +5,19 @@ from .util import run as _run
 
 @click.command()
 @click.option(
-    "-v",
-    "--verbose",
-    is_flag=True,
-    default=False,
-    help="Print detailed build and installation output",
-)
-@click.option(
     "--editable/--no-editable",
+    default=True,
+    help="Install in editable mode.",
+)
+@click.option("-v", "--verbose", is_flag=True, help="Print detailed build output.")
+@click.option(
+    "--verbose-import/--no-verbose-import",
     is_flag=True,
     default=True,
-    help="Install in editable mode",
+    help="Meson only: importing an editable install may trigger a build. This flag determines whether to print that build's output.",
 )
 @click.argument("pip_args", nargs=-1)
-def install(pip_args, verbose, editable):
+def install(*, pip_args, editable, verbose, verbose_import):
     """💽 Build and install package using pip.
 
     By default, the package is installed in editable mode.
@@ -34,14 +33,14 @@ def install(pip_args, verbose, editable):
     pip_args = list(pip_args)
     pip_cmd = ["pip", "install"]
     pip_args += ["--no-build-isolation"]
+
     if editable:
         pip_args += ["--editable"]
-        if verbose:
-            # In case meson is used to install the editable hook, tell it to
-            # print verbose output each time an import triggers a build.
-            # This does not seem to affect setuptools builds.
+
+        if verbose_import:
             pip_args = ["--config-settings=editable-verbose=true"] + pip_args
 
-    pip_args = (["-v"] if verbose else []) + pip_args
+    if verbose:
+        pip_args = ["-v"] + pip_args
 
     _run(pip_cmd + pip_args + ["."], sys_exit=False, replace=True)
