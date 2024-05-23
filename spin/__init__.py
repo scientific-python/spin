504c30b09b8e575adcdd67d681472ac4d7fcd2e3
Jarrod Millman <jarrod.millman@gmail.com>
2024-05-23 16:15:46 -0700

    Bump version

--- spin/__init__.py
+++ spin/__init__.py
@@ -6,7 +6,7 @@ For usage information see `python -m spin`.
 Read more at https://github.com/scientific-python/spin
 """
 
-__version__ = "0.10"
+__version__ = "0.11rc0.dev0"
 
 from .cmds import util
 
