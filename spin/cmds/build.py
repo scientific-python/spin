89c4ad8898f82da52dc6306034cb217ed3accb79
Stefan van der Walt <stefanv@berkeley.edu>
2024-08-27 10:44:17 +0200

    sdist: allow passing args to pyproject-build

--- spin/cmds/build.py
+++ spin/cmds/build.py
@@ -4,6 +4,12 @@ from .util import run
 
 
 @click.command()
-def sdist():
-    """📦 Build a source distribution in `dist/`"""
-    run(["pyproject-build", ".", "--sdist"])
+@click.argument("pyproject-build-args", metavar="", nargs=-1)
+def sdist(pyproject_build_args):
+    """📦 Build a source distribution in `dist/`
+
+    Extra arguments are passed to `pyproject-build`, e.g.
+
+      spin sdist -- -x -n
+    """
+    run(["pyproject-build", ".", "--sdist"] + list(pyproject_build_args))
